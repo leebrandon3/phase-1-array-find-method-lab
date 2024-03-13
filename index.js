d626c464198e5be46e9f1d299b60088756b0448e
Brandon <brandonslee3@gmail.com>
2024-03-13 13:16:14 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(plays){
+    const win = plays.find((element, index, array) => element.result === "W")
+    if (typeof win === "undefined"){
+        return win;
+    }
+    else{
+        return win.year
+    }
+}
